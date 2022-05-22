79c091601d5694da9e3de95d49aa9eeb2a1c43d1
Anushek Prasal <anushekprasal@gmail.com>
2022-05-22 22:59:58 +0530

    KeyHandler: Remove customizations
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- KeyHandler/src/org/lineageos/settings/device/KeyHandler.kt
+++ KeyHandler/src/org/lineageos/settings/device/KeyHandler.kt
@@ -5,7 +5,6 @@
 
 package org.lineageos.settings.device
 
-import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -25,7 +24,6 @@ import java.io.File
 class KeyHandler(context: Context) : DeviceKeyHandler {
     private val audioManager = context.getSystemService(AudioManager::class.java)
     private val inputManager = context.getSystemService(InputManager::class.java)
-    private val notificationManager = context.getSystemService(NotificationManager::class.java)
     private val vibrator = context.getSystemService(Vibrator::class.java)
 
     private val packageContext = context.createPackageContext(
@@ -80,20 +78,12 @@ class KeyHandler(context: Context) : DeviceKeyHandler {
         }
     }
 
-    private fun handleMode(position: Int) {
+    private fun handleMode(mode: Int) {
         val muteMedia = Settings.System.getInt(getContentResolver(),
                 Settings.System.ALERT_SLIDER_MUTE_MEDIA, 0) == 1
 
-        val mode = when (position) {
-            POSITION_TOP -> sharedPreferences.getString(ALERT_SLIDER_TOP_KEY, "0")!!.toInt()
-            POSITION_MIDDLE -> sharedPreferences.getString(ALERT_SLIDER_MIDDLE_KEY, "1")!!.toInt()
-            POSITION_BOTTOM -> sharedPreferences.getString(ALERT_SLIDER_BOTTOM_KEY, "2")!!.toInt()
-            else -> return
-        }
-
         when (mode) {
             AudioManager.RINGER_MODE_SILENT -> {
-                notificationManager.setZenMode(Settings.Global.ZEN_MODE_OFF, null, TAG)
                 audioManager.setRingerModeInternal(mode)
                 if (muteMedia) {
                     audioManager.adjustVolume(AudioManager.ADJUST_MUTE, 0)
@@ -101,15 +91,7 @@ class KeyHandler(context: Context) : DeviceKeyHandler {
                 }
             }
             AudioManager.RINGER_MODE_VIBRATE, AudioManager.RINGER_MODE_NORMAL -> {
-                notificationManager.setZenMode(Settings.Global.ZEN_MODE_OFF, null, TAG)
                 audioManager.setRingerModeInternal(mode)
-                if (muteMedia && wasMuted) {
-                    audioManager.adjustVolume(AudioManager.ADJUST_UNMUTE, 0)
-                }
-            }
-            ZEN_PRIORITY_ONLY, ZEN_TOTAL_SILENCE, ZEN_ALARMS_ONLY -> {
-                audioManager.setRingerModeInternal(AudioManager.RINGER_MODE_NORMAL)
-                notificationManager.setZenMode(mode - ZEN_OFFSET, null, TAG)
                 if (muteMedia && wasMuted) {
                     audioManager.adjustVolume(AudioManager.ADJUST_UNMUTE, 0)
                 }
@@ -122,21 +104,9 @@ class KeyHandler(context: Context) : DeviceKeyHandler {
         private const val TAG = "KeyHandler"
 
         // Slider key positions
-        private const val POSITION_TOP = 1
-        private const val POSITION_MIDDLE = 2
-        private const val POSITION_BOTTOM = 3
-
-        // Preference keys
-        private const val ALERT_SLIDER_TOP_KEY = "config_top_position"
-        private const val ALERT_SLIDER_MIDDLE_KEY = "config_middle_position"
-        private const val ALERT_SLIDER_BOTTOM_KEY = "config_bottom_position"
-        private const val MUTE_MEDIA_WITH_SILENT = "config_mute_media"
-
-        // ZEN constants
-        private const val ZEN_OFFSET = 2
-        private const val ZEN_PRIORITY_ONLY = 3
-        private const val ZEN_TOTAL_SILENCE = 4
-        private const val ZEN_ALARMS_ONLY = 5
+        private const val POSITION_TOP = 0
+        private const val POSITION_MIDDLE = 1
+        private const val POSITION_BOTTOM = 2
 
         // Vibration effects
         private val MODE_NORMAL_EFFECT = VibrationEffect.get(VibrationEffect.EFFECT_HEAVY_CLICK)
